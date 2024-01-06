cd1a27b3db4cc7d7f8b95c0ced5ba6e5c85e4a40
Artur Fedcenco <developerar4y@gmail.com>
2024-01-06 22:24:12 +0200

    e12

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+
 import styles from './page.module.css'
 import { HeaderMenu } from './components/HeaderMenu'
 import { BadgeCard } from './components/BadgeCard'
